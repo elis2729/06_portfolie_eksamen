56d8d2a3d81a4aa2dcccebf0fbf9be7826879f65
elis2729 <Bassen7c>
2021-01-02 11:07:53 +0100

    menu funktionel

---
+++ style.js
@@ -0,0 +1,19 @@
+window.addEventListener("load", sidenVises)
+
+function sidenVises() {
+	console.log("sidenVises");
+	document.querySelector(".burger").addEventListener("click", toggleMenu)
+}
+
+function toggleMenu() {
+	console.log("toggleMenu")
+	document.querySelector(".nav_links").classList.toggle("nav_active");
+
+	let erSkjult = document.querySelector(".nav_links").classList.contains("nav_active")
+
+	if (erSkjult == true) {
+		document.querySelector(".burger").textContent = "X";
+	} else {
+		document.querySelector(".burger").textContent = "☰";
+	}
+}
